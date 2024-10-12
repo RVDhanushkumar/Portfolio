5928a839b77a4713bfb4f0985f16d6a54b485da1
RVDhanushkumar <rapetidhanushkumar13@gmail.com>
2024-10-12 19:03:07 +0530

    add

--- src/App.js
+++ src/App.js
@@ -4,15 +4,24 @@ import FirstPage from './components/first_page.js';
 import Works from './components/works.js';
 import AboutMe from './components/aboutMe.js';
 import Skills from './components/Skills.js';
+import { Element } from 'react-scroll';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <FirstPage />
-      <Works />
-      <AboutMe />
-      <Skills />
+      <Element name="firstPage">
+        <FirstPage />
+      </Element>
+      <Element name="works">
+        <Works />
+      </Element>
+      <Element name="aboutMe">
+        <AboutMe />
+      </Element>
+      <Element name="skills">
+        <Skills />
+      </Element>
     </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,36 @@
-import "../Styling/header.css"
-import pic from "../assets/DK logo.png"
-function Header(){
-    const options=["Home","Projects","About me", "Contact"]
-    const list = options.map(options =><li>{options}</li>)
-    return(
-        <div className = "main_head">
-        <img className="logo" alt="Logo" src={pic}></img>
-        <div className="option">
-            <ul>
-                {list}
-            </ul>
-        </div>
-        </div>
-    );
+// components/header.js
+import React from 'react';
+import { Link } from 'react-scroll';
+import "../Styling/header.css";
+import pic from "../assets/DK logo.png";
+
+function Header() {
+  const options = [
+    { name: "Home", to: "firstPage" },
+    { name: "Projects", to: "works" },
+    { name: "About me", to: "aboutMe" },
+    { name: "Skills", to: "skills" }
+  ];
+
+
+  const list = options.map(option => (
+    <li key={option.to}>
+      <Link to={option.to} smooth={true} duration={500}>
+        {option.name}
+      </Link>
+    </li>
+  ));
+
+  return (
+    <div className="main_head">
+      <img className="logo" alt="Logo" src={pic} />
+      <div className="option">
+        <ul>
+          {list}
+        </ul>
+      </div>
+    </div>
+  );
 }
+
 export default Header;
