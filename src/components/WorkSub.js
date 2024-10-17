c6bd6cbf89cd560ccae72591dad867e8af0aaed5
RVDhanushkumar <rapetidhanushkumar13@gmail.com>
2024-10-17 14:38:07 +0530

    new

--- src/components/WorkSub.js
+++ src/components/WorkSub.js
@@ -1,51 +1,53 @@
-import { width } from "@fortawesome/free-brands-svg-icons/fa42Group";
-import "../Styling/WorkSub.css"
+import "../Styling/cardst.css";
 
-function WorksSub(props){
+function WorksSub(props) {
     const works_sub = {
-        color : "white",
-        display:"flex",
-        justifyContent : "center"
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
     };
-    const info={
+    const info = {
         paddingTop: "200px",
-        marginRight : "100px",
-        marginLeft :"-100px",
-        textAlign : "start",
+        marginRight: "100px",
+        marginLeft: "-100px",
+        textAlign: "start",
     };
     const line = {
         width: "2px",
         height: "600px",
         backgroundColor: "silver",
-        marginRight : "100px",
-        boxShadow : "0px 0px 20px white"
-    }
+        marginRight: "100px",
+        boxShadow: "0px 0px 20px white",
+    };
     const btn = {
-        color : "black",
-        fontWeight : "300",
-        backgroundColor : "white",
-        borderRadius : "10px",
-        padding : "10px",
-    }
-    const pic ={
+        color: "black",
+        fontWeight: "300",
+        backgroundColor: "white",
+        borderRadius: "10px",
+        padding: "10px",
+    };
+    const pic = {
         position: "absolute",
-        paddingTop :"100px",
-        maxHeight : "400px",
+        paddingTop: "100px",
+        maxHeight: "400px",
         maxWidth: "600px",
-        rigth : "100px"
+        right: "100px",
     };
     const h = {
-        maxWidth : "180px",
+        maxWidth: "180px",
     };
-    return(
+
+    return (
         <div className="works_sub" style={works_sub}>
-            <div className="info" style={info} >
+            <div className="info" style={info}>
                 <h1 style={h}>{props.name}</h1>
-                <a href={props.link}><h2 style={btn}>See this Project</h2></a>
+                <a href={props.link}>
+                    <h2 style={btn}>See this Project</h2>
+                </a>
             </div>
-            <div class="vertical-line" style={line}></div>
+            <div className="vertical-line" style={line}></div>
             <div className="pro">
-                <img src= {props.pic} alt="project pic" style={pic}></img>
+                <img src={props.pic} alt="project pic" style={pic}></img>
             </div>
         </div>
     );
--- src/components/works.js
+++ src/components/works.js
@@ -1,37 +1,39 @@
 import WorksSub from "./WorkSub";
 import "../Styling/works.css";
-import pic from "../assets/WomenSadetyApp.png"
-import pic2 from "../assets/SRA.png"
-import pic4 from "../assets/LandingPage.png"
-import pic3 from "../assets/ToDoList.png"
+import pic from "../assets/WomenSadetyApp.png";
+import pic2 from "../assets/SRA.png";
+import pic4 from "../assets/LandingPage.png";
+import pic3 from "../assets/ToDoList.png";
 
 function Works() {
     const workMain = {
-        marginTop : "50px",
-        textAlign : "center",
-        justifyContent : "center",
-        color : "white",
+        marginTop: "50px",
+        textAlign: "center",
+        justifyContent: "center",
+        color: "white",
     };
     const heading = {
-        marginTop : "150px",
-        fontSize : "2.5rem",
-        marginLeft:"80px"
+        marginTop: "150px",
+        fontSize: "2.5rem",
+        marginLeft: "80px",
     };
     const pp = {
-        marginTop : "-20px",
-        marginBottom :"100px",
-    }
+        marginTop: "-20px",
+        marginBottom: "100px",
+    };
 
     return (
         <div className="Works_main" style={workMain}>
-            <h1 style={{fontSize : "1rem"}}> -- Scoll Down To know more --</h1>
+            <h1 style={{ fontSize: "1rem" }}> -- Scroll Down To Know More --</h1>
             <h1 className="work" style={heading}>Works</h1>
-            <p className = "work" style={pp}>Lets check out some of my Top Projects</p>
+            <p className="work" style={pp}>Let's check out some of my Top Projects</p>
             <WorksSub name="Women Safety App" link="https://github.com/RVDhanushkumar/WomenSafetyApp-Evasafe.git" pic={pic} />
             <WorksSub name="Student Registration Management" link="https://github.com/RVDhanushkumar/Student-Registration-Application.git" pic={pic2} />
             <WorksSub name="To-Do List Webpage" link="https://github.com/RVDhanushkumar/To-do-List.git" pic={pic3} />
-            <WorksSub name="Car Rental Landing page" link="https://github.com/RVDhanushkumar/Landing-Page.git" pic={pic4} />
-            <a href="https://github.com/RVDhanushkumar?tab=repositories"><div className="button"> See more </div></a>
+            <WorksSub name="Car Rental Landing Page" link="https://github.com/RVDhanushkumar/Landing-Page.git" pic={pic4} />
+            <a href="https://github.com/RVDhanushkumar?tab=repositories">
+                <div className="button"> See more </div>
+            </a>
         </div>
     );
 }
